76f95bf52561725eb42f7b266e8e21a474ad808c
WiktorZajwij <wiktorworkemail@gmail.com>
2023-04-23 23:22:37 +0200

    Add DreamshareForPug

---
+++ DreamshareForPug/gulp/tasks/scss.js
@@ -0,0 +1,72 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css'; // Сжатие css файла
+import webpcss from 'gulp-webpcss'; // Вывод webp изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Групировка медиа запросов
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+  return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "SCSS",
+        message: "Error: <%= error.message %>"
+      }))
+    )   
+    .pipe(app.plugins.replace(/@img\//g, '../img/'))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+// 
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        groupCssMediaQueries()
+      )
+    )
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        webpcss(
+          {
+            webpClass: '.webp',
+            noWebpClass: '.no-webp'
+          }
+        )
+      )
+    )
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        autoprefixer(
+          {
+            gird: true,
+            overrideBrowserslist: ["last 3 versions"],
+            cascade: true
+          }
+        )
+      )
+    )
+
+    
+    
+    // Раскомментировать если нужен не сжатый дубль файла стилей
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        cleanCss()
+      )
+    )
+    // .pipe(cleanCss())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream());
+}
