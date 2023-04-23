ddafd9139313e0c4f5e924e55d2d647ec1dcca12
WiktorZajwij <wiktorworkemail@gmail.com>
2023-04-23 23:20:54 +0200

    Add DreamshareForHtml

---
+++ DreamshareForHtml/gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+  host: "ftp.dlptest.com", // Адрес FTP сервера
+  user: "dlpuser", // Имя пользователя
+  password: "rNrKYTX9g7z3RgJRmxWuGHbeu", // Пароль
+  parallel: 5 // Кол-во одновременных потоков
+}
